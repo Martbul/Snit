988762771981ab60ccf316251dd3d30debbc780c
Martbul <martbul01@gmail.com>
2024-06-10 23:34:01 +0300

    add llama-3

--- mobileClient/aiModels/langchain.js
+++ mobileClient/aiModels/langchain.js
@@ -21,19 +21,94 @@
 // };
 
 
-export const generateText = async (prompt)=> {
-  const data =  'https://firebasestorage.googleapis.com/v0/b/snit-82af4.appspot.com/o/images%2F1717654155537Screenshot_20240605_182302_Chrome.jpg?alt=media&token=03e028d5-cc68-4088-a102-609b2c4b17dc'
-  const response = await fetch(
-    "https://api-inference.huggingface.co/models/nlpconnect/vit-gpt2-image-captioning",
-    {
-      headers: {
-        Authorization: "Bearer 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",
-      },
-      method: "POST",
-      body: data,
+// export const generateText = async (images) => {
+//   console.log(images);
+//   const data =  'https://firebasestorage.googleapis.com/v0/b/snit-82af4.appspot.com/o/images%2F1717654155537Screenshot_20240605_182302_Chrome.jpg?alt=media&token=03e028d5-cc68-4088-a102-609b2c4b17dc'
+//   const response = await fetch(
+//     "https://api-inference.huggingface.co/models/nlpconnect/vit-gpt2-image-captioning",
+//     {
+//       headers: {
+//         Authorization: "Bearer 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",
+//       },
+//       method: "POST",
+//       body: data,
+//     }
+//   );
+//    const result = await response.json();
+//    console.log(result);
+//   return result;
+// }
+// export const generateText = async (images) => {
+//   try {
+//     const prompt = {
+//       images: images,
+//       messages: [
+//         {
+//           System:
+//             "You are an intelegent car dealer and know everything about cars. These are pictures of cars. I want you to tell me what is this car, model of the car, how old it is, and as much data about it as possible",
+//         },
+//       ],
+//       temperature: 0.5,
+//     };
+
+//     const response = await fetch(
+//       "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct",
+//       {
+//         headers: {
+//           Authorization: "Bearer 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",
+//           "Content-Type": "application/json",
+//         },
+//         method: "POST",
+//         body: JSON.stringify({
+//           inputs: "Can you please let us know more details about your ",
+//         }),
+//       }
+//     );
+
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch data from the server", response);
+//     }
+
+//     const result = await response.json();
+//     console.log(result);
+//     return result;
+//   } catch (error) {
+//     console.error("Error:", error.message);
+//     return { error: error.message };
+//   }
+// };
+
+export const generateText = async (images) => {
+  try {
+    const prompt = {
+      inputs:
+        "Can you please let us know more details about your car? You are an intelligent car dealer and know everything about cars. These are pictures of cars. I want you to tell me what is this car, model of the car, how old it is, and as much data about it as possible.",
+      images: images,
+      temperature: 0.5,
+    };
+
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct",
+      {
+        headers: {
+          Authorization: "Bearer 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",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(prompt),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data from the server");
     }
-  );
-   const result = await response.json();
-   console.log(result);
-  return result;
-}
+
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error("Error:", error.message);
+    return { error: error.message }; // You can handle the error as per your requirements
+  }
+};
+
--- mobileClient/components/homeScreen/CarValuation.jsx
+++ mobileClient/components/homeScreen/CarValuation.jsx
@@ -9,13 +9,13 @@ export const CarValuation = ({ selectedKnowedgeBase }) => {
     const [aiResponse, setAiResponse] = useState(null);
     let { title, images } = selectedKnowedgeBase;
   
-   let images1 =
-     "https://firebasestorage.googleapis.com/v0/b/snit-82af4.appspot.com/o/images%2F1717654155537Screenshot_20240605_182302_Chrome.jpg?alt=media&token=03e028d5-cc68-4088-a102-609b2c4b17dc";
+  //  let images1 =
+  //    "https://firebasestorage.googleapis.com/v0/b/snit-82af4.appspot.com/o/images%2F1717654155537Screenshot_20240605_182302_Chrome.jpg?alt=media&token=03e028d5-cc68-4088-a102-609b2c4b17dc";
   
   useEffect(() => {
-    console.log(images1);
+    // console.log(images1);
     const AIresponse = async () => {
-      const result = await generateText(images1);
+      const result = await generateText(images);
       console.log(result);
       setAiResponse(result);
     };
